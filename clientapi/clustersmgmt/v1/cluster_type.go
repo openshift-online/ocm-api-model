e1d647c4e7e7eb8b8c7fef6018ce23e625b228c7
igavra <igavra@redhat.com>
2025-12-18 20:05:34 +0200

    OCM-11133 | feat: optimize ambiguity of etcd encryption for rosa hcp

--- clientapi/clustersmgmt/v1/cluster_type.go
+++ clientapi/clustersmgmt/v1/cluster_type.go
@@ -757,6 +757,8 @@ func (o *Cluster) GetDomainPrefix() (value string, ok bool) {
 //
 // Indicates whether that etcd is encrypted or not.
 // This is set only during cluster creation.
+// For ROSA-HCP Clusters, etcd is always encrypted, if not set/false, or kms user's key not set,
+// defaults true indicates 'encrypted by internal key'.
 // For ARO-HCP Clusters, this is a readonly attribute, always set to true.
 func (o *Cluster) EtcdEncryption() bool {
 	if o != nil && len(o.fieldSet_) > 26 && o.fieldSet_[26] {
@@ -770,6 +772,8 @@ func (o *Cluster) EtcdEncryption() bool {
 //
 // Indicates whether that etcd is encrypted or not.
 // This is set only during cluster creation.
+// For ROSA-HCP Clusters, etcd is always encrypted, if not set/false, or kms user's key not set,
+// defaults true indicates 'encrypted by internal key'.
 // For ARO-HCP Clusters, this is a readonly attribute, always set to true.
 func (o *Cluster) GetEtcdEncryption() (value bool, ok bool) {
 	ok = o != nil && len(o.fieldSet_) > 26 && o.fieldSet_[26]
